b1847db08ffd548e0bc257699f9fd5ba81bb2b16
Azerus96 <gencinski1996@gmail.com>
2025-03-05 23:27:25 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# GitHub settings
+GITHUB_USERNAME = "Azerus96"  # Замените на свой username
+GITHUB_REPOSITORY = "grofc"  # Замените на имя своего репозитория
+AI_PROGRESS_FILENAME = "cfr_data.pkl"
+AI_PROGRESS_TOKEN = "" # Заполняется в Google Colab
+
+# Training settings (for train_in_colab.py)
+ITERATIONS = 500000
+STOP_THRESHOLD = 0.0001
+SAVE_INTERVAL = 100  # Как часто сохранять прогресс
